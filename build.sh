f64960df0416f4d644034710dd302403d68c0557
mba <krtmarkus@gmail.com>
2021-09-13 15:56:08 +0200

    build last version

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 #clean folder
 rm -rf Backend RelevaRetargeting.zip
-rsync -a --exclude=nbproject --exclude=.git --exclude=.gitignore --exclude=.gitmodules --exclude=./Backend/ --exclude=RelevaRetargeting.zip --exclude=.directory --exclude=build.sh ./ ./Backend/
+rsync -a --exclude=nbproject --exclude=.git --exclude=.gitignore --exclude=.gitmodules --exclude=./Backend/ --exclude=README.md --exclude=RelevaRetargeting.zip --exclude=.directory --exclude=build.sh ./ ./Backend/
 
 #install
 composer install --no-dev -n -o -d Backend/Relevanz
